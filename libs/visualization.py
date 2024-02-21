7ece85743ba8deda06b6710c833bc09fe8368f37
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2024-02-21 14:05:37 -0500

    Fixing NeuralProphet visualization

--- libs/visualization.py
+++ libs/visualization.py
@@ -1,35 +1,32 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import RendererAgg
 
 
 def plot_predictions(ui_params, model, prediction):
-    _lock = RendererAgg.lock
-    with _lock:
+    if ui_params.model == "fbprophet":
         model.plot(prediction)
         plt.title("Prediction")
         plt.xlabel("Date")
         plt.ylabel(ui_params.price_column + " Price")
         # plt.show()
         st.pyplot(plt)
+    elif ui_params.model == "neuralprophet":
+        plot = model.plot(prediction)
+        st.plotly_chart(plot)
 
 
 def plot_fbprophet_components(ui_params, model, prediction):
     if ui_params.model == "fbprophet":
         fig_components = model.plot_components(prediction)
 
-        _lock = RendererAgg.lock
-        with _lock:
-            st.pyplot(fig_components)
+        st.pyplot(fig_components)
 
     elif ui_params.model == "neuralprophet":
         fig_components = model.plot_components(prediction)
         fig_parameters = model.plot_parameters()
 
-        _lock = RendererAgg.lock
-        with _lock:
-            st.plotly_chart(fig_components)
-            if fig_parameters:
-                st.plotly_chart(fig_parameters)
+        st.plotly_chart(fig_components)
+        if fig_parameters:
+            st.plotly_chart(fig_parameters)
 
 
