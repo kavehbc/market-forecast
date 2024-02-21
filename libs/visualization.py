ac9dbff441823a23b9feba6ccc09c9564d349308
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2024-02-20 20:29:30 -0500

    NeuralProphet plot fix

--- libs/visualization.py
+++ libs/visualization.py
@@ -19,7 +19,7 @@ def plot_fbprophet_components(ui_params, model, prediction):
         fig_components = model.plot_components(prediction)
         fig_parameters = None
     elif ui_params.model == "neuralprophet":
-        fig_components = model.plot_components(prediction, residuals=True)
+        fig_components = model.plot_components(prediction)
         fig_parameters = model.plot_parameters()
 
     _lock = RendererAgg.lock
