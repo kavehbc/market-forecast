db188b4e887d148740383d9e0560fa44279b8c9f
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2024-02-21 21:05:29 -0500

    Added License to About section

--- libs/readme.py
+++ libs/readme.py
@@ -7,6 +7,7 @@ def show_readme():
     current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     parent_dir = os.path.dirname(current_dir)
     markdown_file_path = parent_dir + "/README.md"
+    license_file_path = parent_dir + "/LICENSE"
 
     if not os.path.exists(markdown_file_path):
         st.error("Markdown file does not exist.")
@@ -15,4 +16,9 @@ def show_readme():
     with open(markdown_file_path, 'r', encoding="utf-8") as outfile:
         markdown_content = outfile.read()
 
+    with open(license_file_path, 'r', encoding="utf-8") as outfile:
+        license_content = outfile.read()
+
     st.markdown(markdown_content, unsafe_allow_html=True)
+    st.header("License")
+    st.markdown(license_content, unsafe_allow_html=True)
