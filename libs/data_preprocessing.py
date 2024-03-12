a3ee21c2ffebbc1b8cc5703d013da5ca2b250e5a
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2024-03-12 13:26:36 -0400

    fixing date/datetime column name

--- libs/data_preprocessing.py
+++ libs/data_preprocessing.py
@@ -1,10 +1,17 @@
+import streamlit as st
+
+
 def prepare_hisotry_for_fbprophet(ui_params, df_history):
     # data preparation
     df_history_prep = df_history.reset_index()
+
+    # selecting the correct date column
+    date_column = "Date" if "Date" in df_history_prep.columns else 'Datetime'
+
     if ui_params.future_volume > 0:
-        data = df_history_prep[["Date", ui_params.price_column, "Volume"]]
+        data = df_history_prep[[date_column, ui_params.price_column, "Volume"]]
     else:
-        data = df_history_prep[["Date", ui_params.price_column]]  # select Date and Price
-    data = data.rename(columns={"Date": "ds", ui_params.price_column: "y"})
+        data = df_history_prep[[date_column, ui_params.price_column]]  # select Date and Price
+    data = data.rename(columns={date_column: "ds", ui_params.price_column: "y"})
 
     return data
